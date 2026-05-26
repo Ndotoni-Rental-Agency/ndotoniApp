9abea2ef98b382179ae5569409542f051c2b5e8c
Emmanuel Makoye <makoye@Emmanuels-MacBook-Air.local>
2026-05-26 06:34:17 -0700

    chore: move documentation .md files to documentation/ folder

---
+++ app.config.ts
@@ -0,0 +1,86 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const googleMapsApiKey =
+    process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || "AIzaSyAmuUwdIwg_Jz6TGqOpzpWDvKl5YdvNP6w";
+
+  return {
+    name: "ndotoni",
+    slug: "ndotoniApp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "ndotoniapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey,
+      },
+      bundleIdentifier: "com.ndotoni.app",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+        NSLocationWhenInUseUsageDescription:
+          "This app needs access to your location to help you set property coordinates on the map.",
+        NSLocationAlwaysAndWhenInUseUsageDescription:
+          "This app needs access to your location to help you set property coordinates on the map.",
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+      },
+      edgeToEdgeEnabled: true,
+      config: {
+        googleMaps: {
+          apiKey: googleMapsApiKey,
+        },
+      },
+      package: "com.ndotoni.app",
+      permissions: ["ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"],
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+      "@react-native-community/datetimepicker",
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission:
+            "Allow $(PRODUCT_NAME) to use your location to help set property coordinates on the map.",
+          locationWhenInUsePermission:
+            "Allow $(PRODUCT_NAME) to use your location to help set property coordinates on the map.",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "b77e1d8b-2cdb-4d4e-aba1-5f755f2bae26",
+      },
+    },
+  };
+};
